a673bf9bca03aa1d9ee9a87a7c2c8fe07540b09e
Ethan Davidson <theejdavidson@gmail.com>
2020-01-27 19:29:36 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,5 +9,8 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  pp directors_database[0][:movies][:title]
+  index = 0
+  while index < directors_database[0].length do
+  puts directors_database[0][index]
+end
 end
